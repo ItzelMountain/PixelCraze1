467139f071559a72df3fc79175ffd19c8238e342
user <user@DESKTOP-O01I2JM>
2024-04-06 13:14:46 -0700

    Add project files.

---
+++ PixelCraze1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PixelCraze1.Startup))]
+namespace PixelCraze1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
